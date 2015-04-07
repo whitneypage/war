d2b55c35c0f795193b5bdea285f1c5ed6ea76a41
whitneypage <wpagegarner@gmail.com>
2015-04-06 19:37:11 -0600

    Not quite done

--- script.js
+++ script.js
@@ -42,18 +42,40 @@ $(document).ready(function() {
 		} 
 		return copy; 
 	}
+
 	
 	//Now call the shuffle function and save the result of what shuffle returns into your deck variable
+
+	var deck = shuffle(deck);
+
 	
 	var cards_player_1 = [];
 	var cards_player_2 = [];
 	// write a function called deal that will evently divide the deck up between the two players
+
+	var deal = function(deck, player1, player2) {
+		for(var i = 0; i > deck.length; i++) {
+			if (deck[i] % 2 === 0) {
+				player1.push(deck[i]);
+			} else {
+				player2.push(deck[i]);
+			}
+
+		}
+	}
 	
 	
 	//create a function (algorithm) called "war" that takes two cards as parameters, compares them and returns a winner. A tie should return false.
-	var war = function(){
-	
-		
+	var player1;
+	var player2;
+	var war = function(card_1, card_2){
+		if (card_1.number > card_2.number) {
+			return player1;
+		} else if (card_1.number < card_2.number) {
+			return player2;
+		} else {
+			return false;
+		}
 	}
 	
 	var advance = function(){
@@ -73,8 +95,20 @@ $(document).ready(function() {
 	//create a play function
 		//compare the cards
 		//give the winner both cards (at end of deck)
+    var winningCard_2 = cards_player_1.shift();
+    var winningCard_1 = cards_player_2.shift();
+
 	var play = function(){
-		
+		var winner = war(card_1, card_2);
+		if (winner === player1) {
+			cards_player_1.push(winningCard_1, card_1);
+		} else if (winner === player2) {
+			cards_player_2.push(winningCard_2, card_2);
+		} else {
+			cards_player_1.push(card_1);
+			cards_player_2.push(card_2);
+		}
+    }	
 		//this function (defined below) will continue to the next turn
 		advance();
 	}
